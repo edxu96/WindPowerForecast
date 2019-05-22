d9c4322c56e6cbb915b5e9bca28708c62eba408a
edxu96 <edxu96@outlook.com>
2019-05-22 17:18:53 +0200

    v1.1

--- FuncPlot.R
+++ FuncPlot.R
@@ -3,3 +3,12 @@
 # author: Edward J. Xu
 # date: May 22th, 2019
 ########################################################################################################################
+plotVecWeight <- function(vecWeight, vecWeightSeason){
+    numTrain <- length(vecWeight)
+    plot(seq(1, numTrain), vecWeight, xlab = 'Series', ylab = 'Weight Value', bty = "n", col = "lightgrey")
+    points(vecKernal, vecKernalValue_2, col = "blue", lwd = 2, type = "b", pch = 18)
+    title(main = "Wind Power Forecast Training Data and Adaptive Model with Sliding SliWin")
+    legend("bottomright", inset = .02, legend = c("Training Data", "Current Model", "One Year Back", "Two Years Back"),
+        col = c("lightgrey", "blue", "red", "forestgreen"), pch = c(1, 18, 18, 18), lty = c(NA, 1, 2, 3),
+        lwd = c(NA, 2, 2, 2))
+}
--- main.R
+++ main.R
@@ -4,62 +4,62 @@
 # version: 3.4
 # setwd("~/Documents/Github/WindPowerPrediction")
 ########################################################################################################################
-# rm(list = ls())
-# library(lubridate)
-# ########################################################################################################################
-# cat("#### 0,  Control Parameters, Data and Functions ################################\n") ##############################
-# ## 0.1,  Control Parameters 1
-# numFold      <- 10   # [number of folds for cross validation]
-numIte       <- 3    # [number of further iterations] if = 1, there is no further iteration to optimize the coeffcients.
-# outputSeries <- 8    # [series number of the output file]
-# wheOutput    <- T    # [whether to output the results]
-# wheVali      <- T    # [whether to validate the result]
-# numConCoef   <- 360  # [number of concentration coefficients]
+rm(list = ls())
+library(lubridate)
+########################################################################################################################
+cat("#### 0,  Control Parameters, Data and Functions ################################\n") ##############################
+## 0.1,  Control Parameters 1
+numFold      <- 10   # [number of folds for cross validation]
+numIte       <- 1    # [number of further iterations] if = 1, there is no further iteration to optimize the coeffcients.
+outputSeries <- 9    # [series number of the output file]
+wheOutput    <- T    # [whether to output the results]
+wheVali      <- F    # [whether to validate the result]
+numConCoef   <- 360  # [number of concentration coefficients]
 if (numIte > 1) {
     wheFurIte <- T
 } else {
     wheFurIte <- F    # [whether do further iterations]
 }
-# ## 0.2, Name of the data files
-# strNameTrain <- "Data/TrainData3.csv"
-# strNamePred  <- "Data/WeatherForecastInput3.csv"
-# strNameVali  <- "Data/TrainData4.csv"  # Data for validation is the tail data in training data in next session
+## 0.2, Name of the data files
+strNameTrain <- "Data/TrainData4.csv"
+strNamePred  <- "Data/WeatherForecastInput4.csv"
+strNameVali  <- "Data/TrainData4.csv"  # Data for validation is the tail data in training data in next session
 source("Data.R")  # All functions needed for Data.R is in FuncData.R
-# ## 0.3, Function Files
+## 0.3, Function Files
 source("FuncCrossVali.R")
 source("FuncLocalReg.R")
 source("FuncSeasonAdap.R")
 source("FuncWindDirec.R")
-# if (wheOutput) {
-#     source("FuncOutput.R")
-# }
-# ## 0.4,  Control Parameters 2
-# deltaKernalSeasonPred <- numPred / 2  # will set the center of prediction period as the main season.
-# # [forward value of kernalSeasonPred] If 10, means kernalSeasonPred = numTrain + 10.
-# cat("################################################################################\n") ##############################
-# cat("#### 1/6,  vecKernal and matWeight for Local Regression ########################\n")
-# source("PreLocalReg.R")
-# cat("################################################################################\n") ##############################
-# cat("#### 2/6,  Prepare Seasonal Adaptive Models ####################################\n")
-# source("PreSeasonAdap.R")
-# cat("################################################################################\n") ##############################
-# cat("#### 3/6,  Cross Validation to Find Optimal Con-Coef for Wind Direction ########\n")
-# cat("---- 3.1,  Benchmark without Con-Coef ------------------------------------------\n")
-# mrmseBenchmark <- crossValid(vecKernal, listVecKernalValue, datfTrain, 10)
-# cat("armseBenchmark =", mrmseBenchmark, "\n")
-# cat("---- 3.2,  First Iteration -----------------------------------------------------\n")
-# listResult <- optimWindDirection(listVecKernalValue, vecKernalSeason, numConCoef, datfTrain)
-# vecCoef <- listResult$par
-# vecObj <- listResult$obj
-# rm(listResult)
-# if (wheOutput) {
-#     outputResult(vecCoef, outputSeries)
-# }
-# cat("aveARMSE = ", (sqrt(sum((vecObj - mrmseBenchmark)^2)) / numConCoef * 100), "%\n", sep = "")
-# # The calculation of averaged improvement is the same as mse
-# # cat("vecCoef = [", paste(vecCoef, collapse = ", "), "]\n", sep = "")  # It's too long to print
-# # cat("vecObj = [", paste(vecObj, collapse = ", "), "]\n", sep = "")  # It's too long to print
-# cat("--------------------------------------------------------------------------------\n") # ----------------------------
+if (wheOutput) {
+    source("FuncOutput.R")
+}
+## 0.4,  Control Parameters 2
+deltaKernalSeasonPred <- numPred / 2  # will set the center of prediction period as the main season.
+# [forward value of kernalSeasonPred] If 10, means kernalSeasonPred = numTrain + 10.
+cat("################################################################################\n") ##############################
+cat("#### 1/6,  vecKernal and matWeight for Local Regression ########################\n")
+source("PreLocalReg.R")
+cat("################################################################################\n") ##############################
+cat("#### 2/6,  Prepare Seasonal Adaptive Models ####################################\n")
+source("PreSeasonAdap.R")
+cat("################################################################################\n") ##############################
+cat("#### 3/6,  Cross Validation to Find Optimal Con-Coef for Wind Direction ########\n")
+cat("---- 3.1,  Benchmark without Con-Coef ------------------------------------------\n")
+mrmseBenchmark <- crossValid(vecKernal, listVecKernalValue, datfTrain, 10)
+cat("armseBenchmark =", mrmseBenchmark, "\n")
+cat("---- 3.2,  First Iteration -----------------------------------------------------\n")
+listResult <- optimWindDirection(listVecKernalValue, vecKernalSeason, numConCoef, datfTrain)
+vecCoef <- listResult$par
+vecObj <- listResult$obj
+rm(listResult)
+if (wheOutput) {
+    outputResult(vecCoef, outputSeries)
+}
+cat("aveARMSE = ", (sqrt(sum((vecObj - mrmseBenchmark)^2)) / numConCoef * 100), "%\n", sep = "")
+# The calculation of averaged improvement is the same as mse
+# cat("vecCoef = [", paste(vecCoef, collapse = ", "), "]\n", sep = "")  # It's too long to print
+# cat("vecObj = [", paste(vecObj, collapse = ", "), "]\n", sep = "")  # It's too long to print
+cat("--------------------------------------------------------------------------------\n") # ----------------------------
 # 3.2,  Further Iterations
 if (wheFurIte) {
     cat("---- 3.3,  Further Iterations --------------------------------------------------\n")
@@ -105,7 +105,7 @@ if (wheFurIte) {
     datfTrain$speed.center <- updateWindSpeedCenter(vecCoef, datfTrain, numConCoef)
     datfPred$speed.center <- updateWindSpeedCenter(vecCoef, datfPred, numConCoef)
 }
-## 4.2,  Kernal for Adaptive Seasonal LR
+## 4.2,  Kernal for Adaptive Seasonal Local Rregression
 kernalSeasonPred <- numTrain + deltaKernalSeasonPred  # [kernal of seasonal adaptive local regression]
 cat("kernalSeasonPred = ", kernalSeasonPred, "\n", sep = "")
 matWeightSeasonPred <- calMatWeightSeasonGaussian(matWeight, datfTrain$series, kernalSeasonPred)
